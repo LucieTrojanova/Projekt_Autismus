3f25175d728a61ff0d04886296eeeadf9d1e0204
Lucie Trojanová <trojanovalucie1@gmail.com>
2023-06-10 12:48:37 +0200

    oprava odkazu na styly

--- src/organizace/index.js
+++ src/organizace/index.js
@@ -1,4 +1,5 @@
-import { karta } from 'karta/index.js';
+import { karta } from './karta/index.js';
+import './style.css';
 
 export const organizace = (props) => {
   document.querySelector('#app').append(karta({}), karta({}));
